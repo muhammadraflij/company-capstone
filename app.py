35094d5d70cdfe651d1f69861ef6aa64f64c80ef
muhammadraflij <mraflijulian47@gmail.com>
2022-06-02 03:13:17 +0700

    flask

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, request, jsonify
-#from flask_ngrok import run_with_ngrok
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from colect import data_by_city, data_by_location
 from list_to_dict import list_to_dict
 
-#!ngrok authtoken "29mT8kj4OLsAwQwkFiaePr4CQ10_6QbJzAjNwjjSr9cTdWP8h"
-
-app = Flask(__name__)
-#run_with_ngrok(app)   
+app = Flask(__name__) 
 
 # load model
 model = load_model("Model/lstm_6_3_e50/my_model.h5")
+
+kota = request.args.get('city', default = "Jakarta", type = str)
+weatherbit_key = request.args.get('key', default = "9308fac32c9d45a8a4ba9dc418df799f", type = str)
+# collect data
+a = data_by_city(kota, weatherbit_key)
 
 @app.route("/")
 def index():
@@ -20,10 +21,7 @@ def index():
 # /by_city?city=Jakarta&key=
 @app.route("/by_city")
 def by_city():
-  kota = request.args.get('city', default = "Jakarta", type = str)
-  weatherbit_key = request.args.get('key', default = "9308fac32c9d45a8a4ba9dc418df799f", type = str)
-  # collect data
-  a = data_by_city(kota, weatherbit_key)
+  
 
   # scaling and reshape
   scaler = MinMaxScaler()
@@ -62,7 +60,12 @@ def by_location():
   return jsonify(predictions)
   #return f"forecast for the next 3 hours in {lat}, {lon} \n\n\n {predictions}"
 
-
+@app.route("/history")
+def by_history():
+  hist = a[3:]
+  hist = hist.values.tolist()
+  
+  return jsonify(hist)
 
 if __name__ == "__main__":
     app.run(debug=True)
